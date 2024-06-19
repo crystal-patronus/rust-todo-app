cc8e22c071597c42b79c135c20b5a809942654c5
crystal-patronus <patronus@gmail.com>
2024-06-19 00:47:33 -0700

    rust-todo-app: add /addtask api using file operation

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,34 @@
 #[macro_use] extern crate rocket;
 
+use std::{fs::OpenOptions, io::Write};
+use rocket::serde::{Deserialize, json::Json};
+
+#[derive(Deserialize)]
+#[serde(crate = "rocket::serde")]
+struct Task<'r> {
+    item: &'r str
+}
+
 #[get("/")]
 fn index() -> &'static str {
     "hello, world!"
 }
 
+#[post("/addtask", data="<task>")]
+fn add_task(task: Json<Task<'_>>) -> &'static str {
+    let mut tasks = OpenOptions::new()
+                        .read(true)
+                        .append(true)
+                        .create(true)
+                        .open("tasks.txt")
+                        .expect("unable to access tasks.txt");
+    let task_item_string = format!("{}\n", task.item);
+    let task_item_bytes = task_item_string.as_bytes();
+    tasks.write(task_item_bytes).expect("unable to write to tasks.txt");
+    "Task added successfully"
+}
+
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+    rocket::build().mount("/", routes![index, add_task])
 }
