c4e1b5c1c1dee9b5f4a9944bc9d1010fbaf09217
crystal-patronus <patronus@gmail.com>
2024-06-19 19:22:46 -0700

    rust-todo-app: add /edittask api using file operation

--- src/main.rs
+++ src/main.rs
@@ -9,13 +9,20 @@ struct Task<'r> {
     item: &'r str
 }
 
+#[derive(Deserialize, Serialize)]
+#[serde(crate = "rocket::serde")]
+struct TaskUpdate<'r> {
+    id: u8,
+    item: &'r str
+}
+
 #[get("/")]
 fn index() -> &'static str {
     "hello, world!"
 }
 
 #[get("/readtasks")]
-fn read_tasks() -> Json<Vec<String>> {
+fn read_task() -> Json<Vec<String>> {
     let tasks = OpenOptions::new()
                     .read(true)
                     .append(true)
@@ -24,7 +31,11 @@ fn read_tasks() -> Json<Vec<String>> {
                     .expect("unable to access tasks.txt");
     let reader = BufReader::new(tasks);
     Json(reader.lines()
-            .map(|line| line.expect("could no read line"))
+            .map(|line| {
+                let line_string: String = line.expect("could not read line");
+                let line_pieces: Vec<&str> = line_string.split(",").collect();
+                line_pieces[1].to_string()
+            })
             .collect())
 }
 
@@ -36,13 +47,50 @@ fn add_task(task: Json<Task<'_>>) -> &'static str {
                         .create(true)
                         .open("tasks.txt")
                         .expect("unable to access tasks.txt");
-    let task_item_string = format!("{}\n", task.item);
+    let reader = BufReader::new(&tasks);
+    let id = reader.lines().count();
+    let task_item_string = format!("{},{}\n", id, task.item);
     let task_item_bytes = task_item_string.as_bytes();
     tasks.write(task_item_bytes).expect("unable to write to tasks.txt");
     "Task added successfully"
 }
 
+#[put("/edittask", data="<task_update>")]
+fn edit_task(task_update: Json<TaskUpdate<'_>>) -> &'static str {
+    let tasks = OpenOptions::new()
+                        .read(true)
+                        .append(true)
+                        .create(true)
+                        .open("tasks.txt")
+                        .expect("unable to access tasks.txt");
+    let mut temp = OpenOptions::new()
+                        .create(true)
+                        .append(true)
+                        .truncate(true)
+                        .open("temp.txt")
+                        .expect("unable to access tasks.txt");
+    
+    let reader = BufReader::new(tasks);
+    for line in reader.lines() {
+        let line_string: String = line.expect("could not read line");
+        let line_pieces: Vec<&str> = line_string.split(",").collect();
+        if line_pieces[0].parse::<u8>().expect("unable to parse id as u8") == task_update.id {
+            let task_items: [&str; 2] = [line_pieces[0], task_update.item];
+            let task = format!("{}\n", task_items.join(","));
+            temp.write(task.as_bytes()).expect("could not write to temp file");
+        }
+        else {
+            let task = format!("{}\n", line_string);
+            temp.write(task.as_bytes()).expect("could not write to temp file");
+        }
+    }
+
+    std::fs::remove_file("tasks.txt").expect("unable to remove tasks.txt");
+    std::fs::rename("temp.txt", "tasks.txt").expect("unable to renmae temp.txt");
+    "Task updated successfully"
+}
+
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index, add_task, read_tasks])
+    rocket::build().mount("/", routes![index, add_task, read_task, edit_task])
 }
