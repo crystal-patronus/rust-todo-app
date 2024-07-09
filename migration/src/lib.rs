b5490b545072a6e85f3e0ea88c366a2dd9e4a003
crystal-patronus <patronus@gmail.com>
2024-07-09 01:53:22 -0700

    rust-todo-app: add login & signup page

--- migration/src/lib.rs
+++ migration/src/lib.rs
@@ -1,6 +1,7 @@
 pub use sea_orm_migration::prelude::*;
 
 mod m20240630_063944_create_tasks_table;
+mod m20240709_062721_create_users_table;
 
 pub struct Migrator;
 
@@ -9,6 +10,7 @@ impl MigratorTrait for Migrator {
     fn migrations() -> Vec<Box<dyn MigrationTrait>> {
         vec![
             Box::new(m20240630_063944_create_tasks_table::Migration),
+            Box::new(m20240709_062721_create_users_table::Migration),
         ]
     }
 }
